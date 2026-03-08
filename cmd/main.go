5e1d117f950c49656a4a195d4b80b121d57b0cf5
sailaubekulyst <ssahharra2099@gmail.com>
2026-03-08 22:38:45 +0500

    Add files via upload

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"fmt"
+
+	"marketflow/internal/application"
+)
+
+func main() {
+	app, err := application.GetApp()
+	if err != nil {
+		fmt.Println("failed to get app")
+		return
+	}
+	app.Run()
+}
